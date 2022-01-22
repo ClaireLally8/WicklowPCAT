b65c2e42ef8c12666bb561a51884420b3331cd79
Claire Lally <38299093+ClaireLally8@users.noreply.github.com>
2022-01-22 14:56:08 +0000

    Created pages

--- app/main.py
+++ app/main.py
@@ -6,4 +6,12 @@ app= Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/about-me')
+def about_me():
+  return render_template('about_me.html')
+
+@app.route('/about-the-arts')
+def arts_about():
+  return render_template('about_arts.html')
+
     
